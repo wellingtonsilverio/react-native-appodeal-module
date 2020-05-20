a50fc7a7eb8d6e48c7e420bdda43e700d64d5b43
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-20 11:53:17 -0300

    comment functions

--- android/src/main/java/com/reactlibrary/AppodealModuleModule.java
+++ android/src/main/java/com/reactlibrary/AppodealModuleModule.java
@@ -3,16 +3,16 @@ package com.reactlibrary;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+//import com.facebook.react.bridge.Callback;
 
-import android.widget.Toast;
+//import android.widget.Toast;
 
 import com.appodeal.ads.Appodeal;
-import com.appodeal.ads.Native;
-import com.appodeal.ads.NativeAd;
-import com.appodeal.ads.NativeAdView;
-import com.appodeal.ads.UserSettings;
-import com.appodeal.ads.utils.Log;
+//import com.appodeal.ads.Native;
+//import com.appodeal.ads.NativeAd;
+//import com.appodeal.ads.NativeAdView;
+//import com.appodeal.ads.UserSettings;
+//import com.appodeal.ads.utils.Log;
 
 public class AppodealModuleModule extends ReactContextBaseJavaModule {
 
@@ -31,30 +31,30 @@ public class AppodealModuleModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void initialize(String APP_KEY) {
         // Add user settings
-        Appodeal.setUserAge(25);
-        Appodeal.setUserGender(UserSettings.Gender.MALE);
-        Appodeal.initialize(this, APP_KEY, Appodeal.NONE, consent);
+//        Appodeal.setUserAge(25);
+//        Appodeal.setUserGender(UserSettings.Gender.MALE);
+        Appodeal.initialize(getCurrentActivity(), APP_KEY, Appodeal.NONE, false);
     }
-
-    @ReactMethod
-    public void initializeRewarded(String APP_KEY) {
-        Appodeal.initialize(this, APP_KEY, Appodeal.REWARDED_VIDEO, consent);
-        Appodeal.setRewardedVideoCallbacks(new AppodealRewardedVideoCallbacks(this));
-    }
-
-    @ReactMethod
-    public void isRewardedVideoLoaded(String APP_KEY) {
-        if (Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)) {
-            Toast.makeText(this, "true", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "false", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @ReactMethod
-    public void rewardedVideoShow(Callback callback) {
-        boolean isShown = Appodeal.show(this, Appodeal.REWARDED_VIDEO);
-
-        callback.invoke(isShown);
-    }
+//
+//    @ReactMethod
+//    public void initializeRewarded(String APP_KEY) {
+//        Appodeal.initialize(this, APP_KEY, Appodeal.REWARDED_VIDEO, consent);
+//        Appodeal.setRewardedVideoCallbacks(new AppodealRewardedVideoCallbacks(this));
+//    }
+//
+//    @ReactMethod
+//    public void isRewardedVideoLoaded(String APP_KEY) {
+//        if (Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)) {
+//            Toast.makeText(this, "true", Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(this, "false", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    @ReactMethod
+//    public void rewardedVideoShow(Callback callback) {
+//        boolean isShown = Appodeal.show(this, Appodeal.REWARDED_VIDEO);
+//
+//        callback.invoke(isShown);
+//    }
 }
