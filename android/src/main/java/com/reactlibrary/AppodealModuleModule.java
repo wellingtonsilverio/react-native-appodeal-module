a8777c7fe506e9dd7664cd7647f5eb518e73007a
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-20 13:29:18 -0300

    Fix function toast without static

--- android/src/main/java/com/reactlibrary/AppodealModuleModule.java
+++ android/src/main/java/com/reactlibrary/AppodealModuleModule.java
@@ -111,7 +111,7 @@ public class AppodealModuleModule extends ReactContextBaseJavaModule {
             showToast(activity, "onRewardedVideoExpired");
         }
 
-        static void showToast(Activity activity, String text) {
+        void showToast(Activity activity, String text) {
             Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
         }
     }
