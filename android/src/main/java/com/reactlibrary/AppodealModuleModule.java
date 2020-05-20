834cb02e52c7dd821264c50756b026219474e4c5
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-05-20 01:20:31 -0300

    Add Appodeal SDK

--- android/src/main/java/com/reactlibrary/AppodealModuleModule.java
+++ android/src/main/java/com/reactlibrary/AppodealModuleModule.java
@@ -5,6 +5,15 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import android.widget.Toast;
+
+import com.appodeal.ads.Appodeal;
+import com.appodeal.ads.Native;
+import com.appodeal.ads.NativeAd;
+import com.appodeal.ads.NativeAdView;
+import com.appodeal.ads.UserSettings;
+import com.appodeal.ads.utils.Log;
+
 public class AppodealModuleModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
@@ -20,12 +29,32 @@ public class AppodealModuleModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void initialize(String key, Callback callback) {
-        callback.invoke("Received key: " + key);
+    public void initialize(String APP_KEY) {
+        // Add user settings
+        Appodeal.setUserAge(25);
+        Appodeal.setUserGender(UserSettings.Gender.MALE);
+        Appodeal.initialize(this, APP_KEY, Appodeal.NONE, consent);
     }
 
     @ReactMethod
-    public void showRewarded(Callback callback) {
-        callback.invoke("show Rewarded");
+    public void initializeRewarded(String APP_KEY) {
+        Appodeal.initialize(this, APP_KEY, Appodeal.REWARDED_VIDEO, consent);
+        Appodeal.setRewardedVideoCallbacks(new AppodealRewardedVideoCallbacks(this));
+    }
+
+    @ReactMethod
+    public void isRewardedVideoLoaded(String APP_KEY) {
+        if (Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)) {
+            Toast.makeText(this, "true", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "false", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @ReactMethod
+    public void rewardedVideoShow(Callback callback) {
+        boolean isShown = Appodeal.show(this, Appodeal.REWARDED_VIDEO);
+
+        callback.invoke(isShown);
     }
 }
